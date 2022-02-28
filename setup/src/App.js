e03359f16f41cd9c06c135530eb64f608a2e6820
farah <farah.fournat@gmail.com>
2022-02-28 14:12:55 +0100

    project completed

--- setup/src/App.js
+++ setup/src/App.js
@@ -12,7 +12,7 @@ const searchUrl = `https://api.unsplash.com/search/photos/`
 function App() {
   const[loading,setLoading] = useState(false);
   const[photos,setPhotos] = useState([]);
-  const[page,setPage] = useState(1);
+  const[page,setPage] = useState(0);
   const[query,setQuery] = useState('')
 
 
@@ -26,14 +26,17 @@ function App() {
       url = `${searchUrl}${clientID}${urlPage}${urlQuery}`
     }
     else{
-       url = `${mainUrl}${clientID}${urlPage}`
+      url = `${mainUrl}${clientID}${urlPage}`
     }
 
     try {
       const response = await fetch(url);
       const data = await response.json()
       setPhotos((oldPhotos)=>{
-        if(query){
+        if(query && page === 1){
+          return data.results
+        }
+        else if(query){
           return [...oldPhotos,...data.results]
         }
         else{
@@ -61,7 +64,7 @@ function App() {
   },[])
   const handleSubmit = ((e)=>{
     e.preventDefault()
-    fetchImages()
+    setPage(1)
   })
   return (
     <main>
